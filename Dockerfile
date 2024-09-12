d718913d8bf1697fd8a78d7b9bff8d21a1f98e63
Aad Versteden <madnificent@gmail.com>
2024-09-12 14:12:02 +0200

    Initial implementation of oe-id-search
    This allows us to search the CSV and return the found IDs.
    
    Co-author: @ZijneDwaasheid

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,3 @@ FROM semtech/mu-javascript-template:1.8.0
 LABEL maintainer="Aad Versteden <madnificent@gmail.com>"
 
 # see https://github.com/mu-semtech/mu-javascript-template for more info
-FROM semtech/mu-javascript-template:1.8.0
-LABEL maintainer="Aad Versteden <madnificent@gmail.com>"
-
-# see https://github.com/mu-semtech/mu-javascript-template for more info
--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-// see https://github.com/mu-semtech/mu-javascript-template for more info
-
 import { app, query, errorHandler } from 'mu';
 
-app.get('/', function( req, res ) {
-  res.send('Hello mu-javascript-template');
-} );
-
-app.use(errorHandler);
-// see https://github.com/mu-semtech/mu-javascript-template for more info
-
-import { app, query, errorHandler } from 'mu';
-
-app.get('/', function( req, res ) {
-  res.send('Hello mu-javascript-template');
+app.get('/', async function( req, res ) {
+  const search = req.query.search;
+  const response = await fetch(`https://inventaris.onroerenderfgoed.be/aanduidingsobjecten.csv?per_pagina=15&tekst=${encodeURIComponent(req.query.search)}`);
+  let text = await response.text();
+  let csvLines = text.split("\n").slice(5);
+  csvLines = csvLines.slice(0, csvLines.length - 1);
+  const ids = csvLines.slice(1).map( (line) => line.split(",")[0] );
+  res.send(JSON.stringify({data: { attributes: { ids }}}));
 } );
 
 app.use(errorHandler);
